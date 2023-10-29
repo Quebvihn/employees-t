45ede20349965dfd3a4a76c15ccfea6ab19df954
Quebvihn <bamedinatorres@gmail.com>
2023-10-29 14:13:26 -0400

    connection to database

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    
+    user: 'root',
+    
+    password: 'rootroot',
+    database: 'employee_tracker_db'
+});
+
+module.exports = db;
