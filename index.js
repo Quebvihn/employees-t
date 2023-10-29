604dd8ea5ffbc91d5468297c82d844a7fc853630
Quebvihn <bamedinatorres@gmail.com>
2023-10-29 14:56:14 -0400

    database connected

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const inquirer = require('inquirer');
 const db = require('./db/connection');
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    employee_tracker();
+});
